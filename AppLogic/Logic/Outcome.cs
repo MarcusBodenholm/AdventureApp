870b6bd0b7cf4ad0003149d1e869742494b65e8a
Shedcape <shedcapeyt@gmail.com>
2023-10-20 14:21:17 +0200

    Created an outcome class for the plan of refactoring the return type from string to outcome class for all involved methods. This would allow easier customization in the future as only a string is currently a limiting factor.

---
+++ AppLogic/Logic/Outcome.cs
@@ -0,0 +1,10 @@
+﻿namespace AppLogic.Logic
+{
+    public class Outcome
+    {
+        public string Message { get; set; } = string.Empty;
+        public string CurrentLocation { get; set; } = string.Empty;
+        public bool HasWon { get; set; } = false;
+        public List<string> IntentoryNames { get; set; } = new List<string>();
+    }
+}
