d540f3c9b151d9c0b9c37fd1cac5e9e81989fd9f
Shedcape <shedcapeyt@gmail.com>
2023-10-26 15:12:54 +0200

    Updated the welcome form.

--- Adventure/Classes/Forms/WelcomeForm.Designer.cs
+++ Adventure/Classes/Forms/WelcomeForm.Designer.cs
@@ -29,8 +29,6 @@
         private void InitializeComponent()
         {
             labelWelcome = new Label();
-            labelEnterName = new Label();
-            textName = new TextBox();
             buttonPlay = new Button();
             buttonLoad = new Button();
             openFileDialog = new OpenFileDialog();
@@ -40,48 +38,31 @@
             // 
             labelWelcome.AutoSize = true;
             labelWelcome.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
-            labelWelcome.Location = new Point(21, 22);
+            labelWelcome.Location = new Point(12, 9);
             labelWelcome.Name = "labelWelcome";
             labelWelcome.Size = new Size(328, 32);
             labelWelcome.TabIndex = 0;
             labelWelcome.Text = "Welcome to the Adventure!";
             // 
-            // labelEnterName
-            // 
-            labelEnterName.AutoSize = true;
-            labelEnterName.Location = new Point(10, 68);
-            labelEnterName.Name = "labelEnterName";
-            labelEnterName.Size = new Size(338, 15);
-            labelEnterName.TabIndex = 1;
-            labelEnterName.Text = "Please enter your name and then press play to begin the game.";
-            // 
-            // textName
-            // 
-            textName.Location = new Point(10, 85);
-            textName.Margin = new Padding(3, 2, 3, 2);
-            textName.Name = "textName";
-            textName.Size = new Size(373, 23);
-            textName.TabIndex = 2;
-            // 
             // buttonPlay
             // 
-            buttonPlay.Location = new Point(10, 110);
+            buttonPlay.Location = new Point(12, 43);
             buttonPlay.Margin = new Padding(3, 2, 3, 2);
             buttonPlay.Name = "buttonPlay";
             buttonPlay.Size = new Size(151, 38);
             buttonPlay.TabIndex = 3;
-            buttonPlay.Text = "Play";
+            buttonPlay.Text = "Start new game";
             buttonPlay.UseVisualStyleBackColor = true;
             buttonPlay.Click += buttonPlay_Click;
             // 
             // buttonLoad
             // 
-            buttonLoad.Location = new Point(223, 110);
+            buttonLoad.Location = new Point(189, 43);
             buttonLoad.Margin = new Padding(3, 2, 3, 2);
             buttonLoad.Name = "buttonLoad";
             buttonLoad.Size = new Size(151, 38);
             buttonLoad.TabIndex = 4;
-            buttonLoad.Text = "Load";
+            buttonLoad.Text = "Load save";
             buttonLoad.UseVisualStyleBackColor = true;
             buttonLoad.Click += buttonLoad_Click;
             // 
@@ -93,14 +74,13 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(395, 159);
+            ClientSize = new Size(350, 87);
             Controls.Add(buttonLoad);
             Controls.Add(buttonPlay);
-            Controls.Add(textName);
-            Controls.Add(labelEnterName);
             Controls.Add(labelWelcome);
             Margin = new Padding(3, 2, 3, 2);
             Name = "WelcomeForm";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "WelcomeForm";
             ResumeLayout(false);
             PerformLayout();
@@ -109,8 +89,6 @@
         #endregion
 
         private Label labelWelcome;
-        private Label labelEnterName;
-        private TextBox textName;
         private Button buttonPlay;
         private Button buttonLoad;
         private OpenFileDialog openFileDialog;
